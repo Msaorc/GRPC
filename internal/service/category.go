b1df747ac170ce614836b9ac3bc15ddc25c2bc5a
Marcos Augusto <msaorc@hotmail.com>
2024-01-17 08:23:52 -0300

    Criação dos metodos responsaveis pelas chamadas GRPC

---
+++ internal/service/category.go
@@ -0,0 +1,36 @@
+package service
+
+import (
+	"context"
+
+	"github.com/Msaorc/GRPC/internal/database"
+	"github.com/Msaorc/GRPC/internal/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{
+		CategoryDB: categoryDB,
+	}
+}
+
+func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+	category, err := c.CategoryDB.Create(in.Name, in.Description)
+	if err != nil {
+		return nil, err
+	}
+
+	categoryResponse := &pb.Category{
+		Id: category.ID,
+		Name: category.Name,
+		Description: category.Description,
+	}
+
+	return &pb.CategoryResponse{
+		Category: categoryResponse,
+	}, nil
+}
