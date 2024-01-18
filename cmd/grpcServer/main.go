d288968597224a01b4b517b007c46d3cac0194a2
Marcos Augusto <msaorc@hotmail.com>
2024-01-18 08:23:06 -0300

    Criação do listener, para disponibilizar nosso servidor grpc

---
+++ cmd/grpcServer/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"database/sql"
+	"net"
+
+	"github.com/Msaorc/GRPC/internal/database"
+	"github.com/Msaorc/GRPC/internal/pb"
+	"github.com/Msaorc/GRPC/internal/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	categoryDB := database.NewCategory(db)
+	categoryService := service.NewCategoryService(*categoryDB)
+	grpcServer := grpc.NewServer()
+	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
+	reflection.Register(grpcServer)
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
+}
